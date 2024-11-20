3a0ad524eccaafc79bfb008914470f92a436f0d2
roundwood-park <jscooke123@outlook.com>
2024-11-20 14:47:45 +0000

    Create crash.js

---
+++ crash.js
@@ -0,0 +1,2 @@
+javascript:alert("Ready? IF NO CLICK OK.");setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1);
+//javascript:setTimeout(() => {while (true) {while(1)location.reload(1)}}, 1000);
